bd1ccc6b09476df56ea94d8107d66db484b75c76
mattgellert <mattgellert@gmail.com>
2017-10-12 18:20:52 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const query = document.querySelectorAll('.ranked-list li')
+  for (let i=0; i < query.length; i++) {
+    query[i].innerHTML = (parseInt(query[i].innerHTML) + n)
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
